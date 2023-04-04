c19ae35131096df8a2aee65787016ce615a61d3a
Havok <rngdhavok@hotmail.com>
2023-04-03 23:42:24 -0400

    handlebars WIP

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,21 +2,23 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 const withAuth = require('../utils/auth');
 
-// get all posts for homepage
+
 router.get('/', async (req, res) => {
   try {
-    // we need to get all Posts and include the User for each (change lines 8 and 9) - DONE!
+    
     const postData = await Post.findAll({
       include: [User],
     });
-    // serialize the data
+    
     const posts = postData.map((post) => post.get({ plain: true }));
-    // we should render all the posts here - DONE!
+   
     res.render('all-posts-admin', { posts, loggedIn: req.session.loggedIn});
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
 
 // get single post
 router.get('/post/:id', withAuth, async (req, res) => {
