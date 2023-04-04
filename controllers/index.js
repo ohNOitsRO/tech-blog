c66c7ed9662cd80d5095715c9e693802dac8af70
Delgado <rxd718@miami.edu>
2023-04-04 13:09:42 -0400

    WIP api routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 
