00cc7522864121e99c8f4002a1ae814e59ae7337
Havok <rngdhavok@hotmail.com>
2023-04-10 17:53:33 -0400

    fixing endpoints

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,6 +3,7 @@ const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
+
 router.get('/', (req, res) => {
     Post.findAll({
             attributes: [
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
+
 router.get('/', (req, res) => {
     User.findAll({
             attributes: { exclude: ['[password'] }
         })
-        .then(dbUserData => res.json(dbUserData))
+        .then(userData => res.json(userData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
